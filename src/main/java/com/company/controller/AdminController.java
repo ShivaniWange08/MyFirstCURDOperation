75ef0719a22f885d27ec4adc001a6d77e7b7d185
shivani Wange <shiavniwange2003@gmail.com>
2025-08-19 15:27:04 +0530

    logger(@Slf4j) is implement and commit in admin packages initially

--- src/main/java/com/company/controller/AdminController.java
+++ src/main/java/com/company/controller/AdminController.java
@@ -2,13 +2,14 @@ package com.company.controller;
 
 import com.company.entity.Admin;
 import com.company.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -18,7 +19,7 @@ public class AdminController {
 
     @PostMapping("/addAdmin")
     public ResponseEntity<String> addAdmin(@RequestBody Admin admin){
-        System.err.println(admin);
+        log.info("Adding new admin: {}", admin);
         adminService.saveAdmin(admin);
 
         String msg = "Heyy..." +admin.getFName() +", Your profile has been successfully added, Thank You!!";
@@ -28,17 +29,20 @@ public class AdminController {
     @GetMapping("/getAllAdmin")
     public ResponseEntity<List<Admin>> getAllDat(){
         List<Admin> adminList = adminService.getAllAdmin();
+        log.info("All {} admins", adminList.size());
         return new ResponseEntity<>(adminList, HttpStatus.OK);
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<Admin> getAdminById(@PathVariable("id") int id){
+        log.info("Fetch admin with ID: {}", id);
        Admin admin = adminService.getAdminById(id);
        return new ResponseEntity<>(admin, HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteAdmin/{id}")
     public ResponseEntity<String> deleteById(@PathVariable ("id") int id){
+        log.info("Delete admin with ID: {}", id);
         String delete = adminService.deleteAdminByIde(id);
         String msg = "Heyy..., Your profile is Deleted, Thank You!!";
         return new ResponseEntity<>(msg, HttpStatus.OK);
--- src/main/java/com/company/serviceimpl/AdminServiceimpl.java
+++ src/main/java/com/company/serviceimpl/AdminServiceimpl.java
@@ -4,11 +4,13 @@ package com.company.serviceimpl;
 import com.company.entity.Admin;
 import com.company.repositories.AdminRepository;
 import com.company.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class AdminServiceimpl implements AdminService {
 
@@ -18,24 +20,28 @@ public class AdminServiceimpl implements AdminService {
     @Override
     public String saveAdmin(Admin admin) {
         Admin saveAdmin = adminRepository.save(admin);
+        log.info("Admin saved successfully with id={}", saveAdmin.getId());
         return "Admin Saved";
     }
 
     @Override
     public List<Admin> getAllAdmin() {
         List<Admin> adminList = adminRepository.findAll();
+        log.info("Fetched {} admins from DB", adminList.size());
         return adminList;
     }
 
     @Override
     public Admin getAdminById(int id) {
         Admin admin = adminRepository.findById(id).orElseThrow(()->new NullPointerException("Admin with id not found" +id));
+        log.info("Fetch admin with id={}", id);
         return admin;
     }
 
     @Override
     public String deleteAdminByIde(int id) {
         adminRepository.deleteById(id);
+        log.info("Deleted admin with id={}", id);
         return "Admin Deleted";
     }
 }
